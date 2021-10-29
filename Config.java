46508719e0337c2476d1551e986f388c6f775c95
Ramishwer <78228371+Ramishwer@users.noreply.github.com>
2021-10-29 10:58:50 +0530

    Create Config.java

---
+++ Config.java
@@ -0,0 +1,16 @@
+package com.aris.SpringcoreAssign;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+@ComponentScan(basePackages="Maven_Demo.Aop_Aspkt")
+public class Config
+{
+
+
+
+}
